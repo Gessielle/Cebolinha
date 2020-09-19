03c040c4fc615b002eb69c495337fb3472ae7975
Gessielle <gessielle-barossi@hotmail.com>
2020-09-19 15:38:39 -0300

    Cebolinha

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string frase;
+
+            Console.Write("Digite qualquer coisa: ");
+            frase = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($"{frase.Replace("r", "l").Replace("R", "L")}");
+
+            Console.WriteLine("Presione Enter para sair");
+            Console.ReadKey();
+
+
+        }
+    }
+}
